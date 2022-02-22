43aeaff074a0e40ed52373b314a8d66189436b54
erwanclx <81480765+erwanclx@users.noreply.github.com>
2022-02-22 13:35:15 +0100

    auto refresh with a 30sec interval, reduce the latency

---
+++ cogs/scrap.py
@@ -0,0 +1,75 @@
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+from discord.ext import commands, tasks
+import tracemalloc
+
+tracemalloc.start()
+
+zones_array = ['NORTH AMERICA WEST', 'NORTH AMERICA EAST', 'EUROPE CENTRAL', 'SOUTH AMERICA', 'EUROPE WEST']
+
+zones_arr = {}
+ts = 0
+
+
+class scrape_loop(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.serveur_scrape.start()
+
+    @tasks.loop(seconds=30)
+    async def serveur_scrape(self):
+        global zones_arr
+        global ts
+
+        zone_class = "ags-ServerStatus-content-responses-response"
+        server_class = "ags-ServerStatus-content-responses-response-server"
+        server_name_class = "ags-ServerStatus-content-responses-response-server-name"
+        server_status_class = "ags-ServerStatus-content-responses-response-server-status"
+
+        URL = "http://playlostark.com/fr-fr/support/server-status"
+        page = requests.get(URL)
+        page
+        page.status_code
+        200
+        page.content
+
+        soup = BeautifulSoup(page.content, "html.parser")
+        zones = soup.find_all("div", class_=zone_class)
+        zones_arr = {}
+
+        for zone in zones:
+            servers = zone.find_all("div", class_=server_class)
+            servers_arr = []
+            for server in servers:
+                name = server.find("div", class_=server_name_class)
+                status = server.find("div", class_=server_status_class)
+                server_status = ""
+                server_obj = {}
+                if ("ags-ServerStatus-content-responses-response-server-status--good") in status.attrs.get("class"):
+                    server_status = "Good"
+                if ("ags-ServerStatus-content-responses-response-server-status--busy") in status.attrs.get("class"):
+                    server_status = "Busy"
+                if ("ags-ServerStatus-content-responses-response-server-status--full") in status.attrs.get("class"):
+                    server_status = "Full"
+                if (("ags-ServerStatus-content-responses-response-server-status--maintenance") in status.attrs.get(
+                        "class")):
+                    server_status = "Maintenance"
+                server_obj['name'] = name.text.replace("\n", "").replace("\r", "").replace(" ", "")
+                server_obj['status'] = server_status
+                servers_arr.append(server_obj)
+            zones_arr[(zones_array[int(zone.attrs.get('data-index'))])] = servers_arr
+        ts = datetime.timestamp(datetime.now())
+        print('refresh')
+        global timer
+        hour = str(datetime.now().hour)
+        minute = datetime.now().minute
+        if minute < 10:
+            minute = "0", str(minute)
+        timer = hour + "h" + str(minute)
+        self.bot.timer = timer
+        self.bot.zones_arr = zones_arr
+
+#
+def setup(bot):
+    bot.add_cog(scrape_loop(bot))
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import discord
-import requests
-from bs4 import BeautifulSoup
 from discord.ext import commands
 import os
-from datetime import datetime
 
 zones_array = ['NORTH AMERICA WEST', 'NORTH AMERICA EAST', 'EUROPE CENTRAL', 'SOUTH AMERICA', 'EUROPE WEST']
 ts = 0
@@ -34,86 +31,25 @@ async def printall(ctx, text):
     if b != "":
         a.append(b)
 
-    if timer == "":
+    if bot.timer == "":
         for t in a:
             await ctx.send(t)
     else:
         for t in a:
-            await ctx.send(t + "\n*Last update : " + timer + "*")
-
+            await ctx.send(t + "\n*Last update : " + bot.timer + "*")
 
 @bot.command()
 async def zones(ctx):
     text = ""
-    global timer
-    timer = ""
     for x in zones_array:
         text += x.title() + '\r\n'
     await printall(ctx, text)
 
-
 @bot.command()
 async def server(ctx, *, entry):
-    global ts
-    global zones_arr
-    if datetime.timestamp(datetime.now()) - ts < (5 * 60 * 1000):
-        zones_arr = []
-
-    zone_class = "ags-ServerStatus-content-responses-response"
-    server_class = "ags-ServerStatus-content-responses-response-server"
-    server_name_class = "ags-ServerStatus-content-responses-response-server-name"
-    server_status_class = "ags-ServerStatus-content-responses-response-server-status"
-
-    URL = "http://playlostark.com/fr-fr/support/server-status"
-    page = requests.get(URL)
-    page
-    page.status_code
-    200
-    page.content
-
-    soup = BeautifulSoup(page.content, "html.parser")
-    zones = soup.find_all("div", class_=zone_class)
-    zones_arr = {}
-
-    for zone in zones:
-        servers = zone.find_all("div", class_=server_class)
-        servers_arr = []
-        for server in servers:
-            name = server.find("div", class_=server_name_class)
-            status = server.find("div", class_=server_status_class)
-            server_status = ""
-            server_obj = {}
-            if ("ags-ServerStatus-content-responses-response-server-status--good") in status.attrs.get("class"):
-                server_status = "Good"
-            if ("ags-ServerStatus-content-responses-response-server-status--busy") in status.attrs.get("class"):
-                server_status = "Busy"
-            if ("ags-ServerStatus-content-responses-response-server-status--full") in status.attrs.get("class"):
-                server_status = "Full"
-            if (("ags-ServerStatus-content-responses-response-server-status--maintenance") in status.attrs.get(
-                    "class")):
-                server_status = "Maintenance"
-            server_obj['name'] = name.text.replace("\n", "").replace("\r", "").replace(" ", "")
-            server_obj['status'] = server_status
-            servers_arr.append(server_obj)
-        zones_arr[(zones_array[int(zone.attrs.get('data-index'))])] = servers_arr
-    ts = datetime.timestamp(datetime.now())
-    print('refresh')
-    print(type(zones_arr))
-    print(zones_arr)
-    global timer
-    hour = str(datetime.now().hour)
-    minute = datetime.now().minute
-    if minute < 10:
-        minute = "0", str(minute)
-    timer = hour + "h" + str(minute)
-    temp_arr = zones_arr
+    temp_arr = bot.zones_arr
     temp_arr = [[x for x in v if x['name'].upper() == entry.upper()] for v in temp_arr.values()]
     temp_arr = [x for x in temp_arr if x]
-    status = dict(
-        Good="✅",
-        Full="❌",
-        Maintenance="⚠️",
-        Busy="⛔")
     if temp_arr:
         temp_arr = temp_arr[0][0]
         text = temp_arr["name"] + ': ' + temp_arr["status"] + " " + status[temp_arr["status"]]
@@ -122,69 +58,11 @@ async def server(ctx, *, entry):
 
     await printall(ctx, text)
 
-
 @bot.command()
 async def zone(ctx, *, entry):
-    global ts
-    global zones_arr
-    if datetime.timestamp(datetime.now()) - ts < (5 * 60 * 1000):
-        zones_arr = []
-
-    zone_class = "ags-ServerStatus-content-responses-response"
-    server_class = "ags-ServerStatus-content-responses-response-server"
-    server_name_class = "ags-ServerStatus-content-responses-response-server-name"
-    server_status_class = "ags-ServerStatus-content-responses-response-server-status"
-
-    URL = "http://playlostark.com/fr-fr/support/server-status"
-    page = requests.get(URL)
-    page
-    page.status_code
-    200
-    page.content
-
-    soup = BeautifulSoup(page.content, "html.parser")
-    zones = soup.find_all("div", class_=zone_class)
-    zones_arr = {}
-
-    for zone in zones:
-        servers = zone.find_all("div", class_=server_class)
-        servers_arr = []
-        for server in servers:
-            name = server.find("div", class_=server_name_class)
-            status = server.find("div", class_=server_status_class)
-            server_status = ""
-            server_obj = {}
-            if ("ags-ServerStatus-content-responses-response-server-status--good") in status.attrs.get("class"):
-                server_status = "Good"
-            if ("ags-ServerStatus-content-responses-response-server-status--busy") in status.attrs.get("class"):
-                server_status = "Busy"
-            if ("ags-ServerStatus-content-responses-response-server-status--full") in status.attrs.get("class"):
-                server_status = "Full"
-            if (("ags-ServerStatus-content-responses-response-server-status--maintenance") in status.attrs.get(
-                    "class")):
-                server_status = "Maintenance"
-            server_obj['name'] = name.text.replace("\n", "").replace("\r", "").replace(" ", "")
-            server_obj['status'] = server_status
-            servers_arr.append(server_obj)
-        zones_arr[(zones_array[int(zone.attrs.get('data-index'))])] = servers_arr
-    ts = datetime.timestamp(datetime.now())
-    print('refresh')
-    print(type(zones_arr))
-    print(zones_arr)
-    global timer
-    hour = str(datetime.now().hour)
-    minute = datetime.now().minute
-    if minute < 10:
-        minute = "0", str(minute)
-    timer = hour + "h" + str(minute)
-    arr = zones_arr
+    arr = bot.zones_arr
     text = ""
     entry = entry.upper()
-    status = dict(
-        Good="✅",
-        Full="❌",
-        Maintenance="⚠️",
-        Busy="⛔")
     if entry in arr.keys():
         for server in arr[entry]:
             text += server["name"] + ": " + server["status"] + " " + status[server["status"]] + "\r\n"
@@ -193,10 +71,9 @@ async def zone(ctx, *, entry):
 
     await printall(ctx, text)
 
-
 @bot.command()
 async def help(ctx):
-    embed = discord.Embed(title="Listes des commandes", description="", color=15277667)
+    embed = discord.Embed(title="Command list :", description="Bot version : v1.2.1", color=15277667)
     embed.add_field(name=".help", value="Show command list", inline=True)
     embed.add_field(name=".zones", value="Show all the zones", inline=True)
     embed.add_field(name=".zone [value]", value="Show the state of the servers of a zone", inline=True)
@@ -205,8 +82,17 @@ async def help(ctx):
     embed.set_thumbnail(url="https://kgo.googleusercontent.com/profile_vrt_raw_bytes_1587514212_1078.jpg")
     await ctx.send(embed=embed)
 
-
 @bot.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Streaming(name=".help to see commands", url='https://github.com/erwanclx'))
+    await bot.change_presence(activity=discord.Streaming(name=".devhelp to see commands", url='https://github.com/erwanclx'))
+
+
+    # -------- LOAD SCRAP FILE -------- #
+
+for filename in os.listdir("./cogs"):
+    if filename.endswith('.py'):
+        try:
+            bot.load_extension(f"cogs.{filename[:-3]}")
+        except Exception as e:
+            print(e)
 bot.run('')
